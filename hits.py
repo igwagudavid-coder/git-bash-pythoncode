2a97fd46c10e45f6ee58868dfc363c9db57b24f4
Igwagu David <igwagudavid@gmail.com>
2026-07-02 15:26:01 +0100

    adding new code file

---
+++ hits.py
@@ -0,0 +1,10 @@
+hits=[]
+while True:
+    artist = input("What's the name of the artist?: ")
+    song = input("What's the name of the song?: ")
+    song_info = {"artist":artist,"song":song}
+    hits.append(song_info)
+    choice = (input("Would you like to add another song? (y/n): ")).lower()
+    if  choice != "y" or choice != "yes":
+        break
+print(f"Your best hits are {hits}")

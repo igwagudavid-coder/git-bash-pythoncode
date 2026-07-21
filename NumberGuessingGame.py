438237622cf7baba6d95004fede8805fc653f3b7
Igwagu David <igwagudavid@gmail.com>
2026-07-21 11:13:54 +0100

    Added a number guessing game with ASCII art logos

---
+++ NumberGuessingGame.py
@@ -0,0 +1,29 @@
+from random import randint
+import guessingGameFunctions
+
+def main():
+    print(guessingGameFunctions.welcome_logo)
+    number = randint(1, 100)
+
+
+    attempts = guessingGameFunctions.pick_level()
+    while attempts > 0:
+        guess = int(input("\nWhat's your guess? :"))
+        correctNumber = guessingGameFunctions.checkGuess(number , guess)
+
+        if correctNumber:
+            print(guessingGameFunctions.win_logo)
+            break
+        attempts -=1
+        if attempts>0 :
+
+
+            print(f"\nYou have {attempts} guesses left! ")
+        else:
+            print("\n\nYou lose!!!")
+            print(f"\nThe number was {number}")
+            print(guessingGameFunctions.lose_logo)
+
+
+if __name__ == "__main__":
+    main()
---
+++ guessingGameFunctions.py
@@ -0,0 +1,52 @@
+def pick_level():
+    pick = input("Pick a level, easy or hard : ")
+    if pick == "easy":
+        tries= 10
+        print("Guess a number between 1 and 100!")
+        print(f"\n\nYou have {tries} tries!")
+        return tries
+
+    elif pick == "hard":
+        tries =5
+        print("Guess a number between 1 and 100!")
+        print(f"\n\nYou have {tries} tries!")
+        return tries
+
+    else: print("\nWrong input!")
+
+def checkGuess(number,guess)-> bool:
+    if guess == number:
+        return True
+    elif   guess > number:
+        print("\nToo high, guess again")
+        return False
+    elif guess < number:
+        print("\nToo low, guess again")
+        return False
+win_logo = r"""
+                                                                      .           
+                                                                    .o8           
+     .ooooo.   .ooooo.  ooo. .oo.    .oooooooo oooo d8b  .oooo.   .o888oo  .oooo.o
+    d88' `"Y8 d88' `88b `888P"Y88b  888' `88b  `888""8P `P  )88b    888   d88(  "8
+    888       888   888  888   888  888   888   888      .oP"888    888   `"Y88b. 
+    888   .o8 888   888  888   888  `88bod8P'   888     d8(  888    888 . o.  )88b
+    `Y8bod8P' `Y8bod8P' o888o o888o `8oooooo.  d888b    `Y888""8o   "888" 8""888P'
+                                    d"     YD                                     
+                                    "Y88888P'                                     
+    """
+lose_logo = r"""
+oooo                                                          .o. .o. .o.
+`888                                                          888 888 888
+ 888   .ooooo.   .oooo.o  .ooooo.  oooo d8b oooo d8b oooo d8b 888 888 888
+ 888  d88' `88b d88(  "8 d88' `88b `888""8P `888""8P `888""8P Y8P Y8P Y8P
+ 888  888   888 `"Y88b.  888ooo888  888      888      888     `8' `8' `8'
+ 888  888   888 o.  )88b 888    .o  888      888      888     .o. .o. .o.
+o888o `Y8bod8P' 8""888P' `Y8bod8P' d888b    d888b    d888b    Y8P Y8P Y8P
+"""
+welcome_logo = r"""
+__        __   _                            _ _ _ 
+\ \      / /__| | ___ ___  _ __ ___   ___  | | | |
+ \ \ /\ / / _ \ |/ __/ _ \| '_ ` _ \ / _ \ | | | |
+  \ V  V /  __/ | (_| (_) | | | | | |  __/ |_|_|_|
+   \_/\_/ \___|_|\___\___/|_| |_| |_|\___| (_|_|_)
+"""

7e036bbf7364dfce7d3770f0b5cbfd3fdb46a5b1
David Igwagu <igwadudavid@gmail.com>
2026-09-05 15:22:00 +0100

    Chore: Corrected a few errors in weather fetch

--- api/weatherDataFetch.py
+++ api/weatherDataFetch.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import ConnectionError,Timeout,HTTPError
 from Data.models.data_model import CityData,WeatherData
 from config.app_config import BASE_DIR
 import json
@@ -29,15 +30,32 @@ class WeatherDataFetch():
         self.buildParams()
         self.buildUrl()
 
-        response = requests.get(url = self.url, params = self.params, timeout = 20)
-        data = response.json()
+        try:
+            response = requests.get(url = self.url, params = self.params, timeout = 20)
+            response.raise_for_status()
+            data = response.json()
+        except ConnectionError as e:
+            print(f"Weather fetching failed: {e}")
+            choice = input("Do you want to continue (y/n)?")
+            if choice == "y":
+                self.fetchWeather()
+            raise ConnectionError("Couldn't connect the weather service (bad internet connection).{Weather Data Fetch}")
+
+        except Timeout as e:
+            print(f"Weather fetching failed:Server timeout")
+            raise Timeout("Server timeout {Weather Data Fetch}")
+
+        except HTTPError as e:
+            print(f"Weather fetching failed: Bad Server Response")
+            raise HTTPError("Bad Server Response. {Weather Data Fetch}")
+
 
         time = data["current"]["time"]
         temp = data["current"]["temperature_2m"]
         rel_h = data["current"]["relative_humidity_2m"]
         wind_speed = data["current"]["wind_speed_10m"]
         weather_status = data["current"]["weather_code"]
-        is_day ="yh" if  data["current"]["is_day"] == 1 else "nh"
+        is_day ="yes" if  data["current"]["is_day"] == 1 else "no"
         return WeatherData(
             time = time,
             temperature= temp,
